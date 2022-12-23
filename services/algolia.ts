5174195ccc0ba0711090edd5688ea53693448fc0
Ayush Soni <ayushsoni1010.work@gmail.com>
2022-12-24 01:33:14 +0530

    update: algolia search

--- services/algolia.ts
+++ services/algolia.ts
@@ -6,25 +6,22 @@ const adminKey: any = process.env.adminApiKey;
 
 const algoliaClient = algoliaSearch(applicationId, adminKey);
 
-const algoliaHelper = algoliaSearchHelper(algoliaClient, "dummy", {
-  disjunctiveFacets: [
-    "food_type",
-    "dining_style",
-    "price_range",
-    "stars_count",
-  ],
-  hitsPerPage: 9,
+const algoliaHelper = algoliaSearchHelper(algoliaClient, "people", {
+  disjunctiveFacets: ["city", "username", "skills"],
+  hitsPerPage: 7,
   maxValuesPerFacet: 20,
   attributesToRetrieve: [
-    "image_url",
+    "id",
     "name",
-    "stars_count",
-    "price_range",
-    "reviews_count",
-    "reserve_url",
-    "food_type",
-    "address",
+    "username",
     "city",
+    "email",
+    "designation",
+    "gender",
+    "image_url",
+    "skills",
+    "color",
+    "date",
     "_geoloc",
   ],
 });
